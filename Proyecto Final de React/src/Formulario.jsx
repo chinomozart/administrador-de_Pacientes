cd95311fa04efe6c472550af71c6598e8bbab79c
chinomozart/Juan Escalante <119026314+chinomozart@users.noreply.github.com>
2023-04-26 10:40:04 -0600

    Fase2 terminada
    Fase dos terminada.

--- Proyecto Final de React/src/Formulario.jsx
+++ Proyecto Final de React/src/Formulario.jsx
@@ -9,15 +9,24 @@ export default function Formulario(){
     const[pacientes,setPacientes]=useState([])
     const[mascota,setMascota]=useState("")
     const[dueño,setDueño]=useState("")
+    const[date,setDate]=useState("")
+    const[hora,setHora]=useState("")
+    const[sintomas,setSintomas]=useState("")
 
     function agregarpaciente(){
         const informacion={
             mascota,
-            dueño
+            dueño,
+            date,
+            hora,
+            sintomas,
         }
-        setPacientes([...pacientes,informacion])
+        setPacientes([...pacientes,informacion,date,hora,sintomas])
         setMascota("")
         setDueño("")
+        setDate("")
+        setHora("")
+        setSintomas("")
     }
 
 
@@ -28,7 +37,14 @@ export default function Formulario(){
             <input onChange={(e)=>{setMascota(e.target.value)}} value={mascota} type="text"></input>
             <label htmlFor="" className="dueño"> Nombre Dueño</label>
             <input onChange={(e)=>{setDueño(e.target.value)}} value={dueño} type="text"></input>
-            <button onClick={agregarpaciente} className="boton1"  type="Submit">Agregar</button>
+            <label htmlFor="" className="date">Fecha</label>
+            <input  className="" onChange={(e)=>{setDate(e.target.value)}} value={date} type="date"></input>
+            <label htmlFor="" className="hora">Hora</label>
+            <input onChange={(e)=>{setHora(e.target.value)}} value={hora} type="time"></input>
+            <label htmlFor="" className="sintomas">Sintomas</label>
+            <input onChange={(e)=>{setSintomas(e.target.value)}} value={sintomas} type="text"></input>
+            <button onClick={agregarpaciente} className="boton1"  type="button">Agregar Cita</button>
+            
 
          </form>
         </div>
